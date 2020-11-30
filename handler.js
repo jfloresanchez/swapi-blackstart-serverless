a5d254ce6a9267dccefae88589b8058ddad67a2d
Jorge Flores <jfloresanchez@gmail.com>
2020-11-30 15:45:44 -0500

    [update]: Agrega nueva funcionalidad de getPeople

--- handler.js
+++ handler.js
@@ -2,7 +2,7 @@
 const swapi = require('swapi-node');
 
 
-module.exports.getSwapi = async (event, context, callback) => {
+module.exports.getSwapiPeoples = async (event, context, callback) => {
   const { page } = event.queryStringParameters
   const peoplesResult = await swapi
     .get(`https://swapi.dev/api/people/?page=${page}`)
@@ -10,6 +10,19 @@ module.exports.getSwapi = async (event, context, callback) => {
   const response = {
     statusCode: 200,
     peoples: peoplesResult
+  };
+
+  return callback(null, response);
+};
+
+module.exports.getSwapiPeople = async (event, context, callback) => {
+  const { id } = event.queryStringParameters
+  const peopleResult = await swapi
+    .getPerson(id)
+    .then((result) => result);
+  const response = {
+    statusCode: 200,
+    peoples: peopleResult
   };
 
   return callback(null, response);
