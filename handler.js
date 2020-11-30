87089a0053d63adbd13f554d37d023efe024b276
Jorge Flores <jfloresanchez@gmail.com>
2020-11-30 15:26:41 -0500

    update: swapi

---
+++ handler.js
@@ -0,0 +1,28 @@
+'use strict';
+const swapi = require('swapi-node');
+
+
+module.exports.getSwapi = async (event, context, callback) => {
+  //const page = req.params.page != null ? req.params.page : 1;
+  //const page = event.page
+  const { page } = event.queryStringParameters
+  const peoplesResult = await swapi
+    .get(`https://swapi.dev/api/people/?page=${page}`)
+    .then((result) => result);
+  const response = {
+    statusCode: 200,
+    peoples: peoplesResult
+  };
+
+  return callback(null, response);
+};
+
+module.exports.getPeoples = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'getSwapi',
+    }),
+  };
+  return callback(null, response);
+};
